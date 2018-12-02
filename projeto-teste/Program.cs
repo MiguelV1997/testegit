0fb69c3fdbc684b95502aeb9fe1b6d974ef69965
Miguel Ventura <MiguelVentura16@hotmail.com>
2018-12-02 15:06:14 +0000

    Acrescentada uma mensagem de bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
+
+            Console.WriteLine("Olá mundo");
+            Console.WriteLine("Bom dia");
+
+            Console.ReadLine();
         }
     }
 }

c2ec607654ebab04060dab1078dda3c344dd3d25
Miguel Ventura <MiguelVentura16@hotmail.com>
2018-12-02 15:25:53 +0000

    Acrescentada uma mensagem desejando bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -9,9 +9,8 @@ namespace projeto_teste {
         static void Main(string[] args) {
 
             Console.WriteLine("Olá mundo");
-            Console.WriteLine("Bom dia");
             Console.WriteLine("Como vc está hoje");
-
+            Console.WriteLine("Tenha um bom dia");
             Console.ReadLine();
         }
     }
